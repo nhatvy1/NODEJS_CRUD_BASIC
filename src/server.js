6fdddf8e19df5da898291cf94f8cb10f23d8bb83
nhatvy1 <86657972+nhatvy1@users.noreply.github.com>
2023-05-16 15:20:13 +0700

    connection pools

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const configViewEngine = require('./configs/viewEngine')
 const express = require('express')
 const webRoutes = require('./routes/web')
-
+const connection = require('./configs/dbConnection')
 const app = express()
 const port = process.env.PORT 
 const hostname = process.env.HOST_NAME
@@ -10,6 +10,17 @@ const hostname = process.env.HOST_NAME
 configViewEngine(app)
 
 app.use('/' ,webRoutes)
+
+
+
+// simple query
+connection.query(
+    'SELECT * FROM Users u',
+    function(err, results, fields) {
+      console.log('Result: ', results); // results contains rows returned by server
+      // console.log('Fields: ', fields); // fields contains extra meta data about results, if available
+    }
+  );
 
 
 app.listen(port, hostname, () => {
